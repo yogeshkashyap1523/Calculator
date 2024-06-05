bf1df6c0b40a1dee834ecbc3d007f8bf22ecdbf1
yogeshkashyap1523 <126580273+yogeshkashyap1523@users.noreply.github.com>
2024-06-05 16:57:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let display = document.getElementById('display');
+
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
+
